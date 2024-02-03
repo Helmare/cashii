09da539e2d58bf03956fa72d1ff9a0a2e4b5fa87
Helmare <h3lmare@gmail.com>
2024-02-03 09:21:34 -0600

    added some basic transfer create, list, and remove commands

---
+++ src/cashii.ts
@@ -0,0 +1,52 @@
+import { Transaction } from "./Transaction.js";
+import { existsSync } from "fs";
+import { readFile, writeFile } from "fs/promises";
+import { join } from "path";
+
+export const DEFAULT_PATH = join(process.cwd(), 'cashii.json');
+export interface Cashii {
+  transactions: Transaction[]
+}
+export const DEFAULT_CASHII: Cashii = {
+  transactions: []
+};
+
+/**
+ * Loads a Cashii object from the path in JSON.
+ * 
+ * @param path 
+ * @returns 
+ */
+export async function load(path?: string): Promise<Cashii> {
+  path = path || DEFAULT_PATH;
+  if (existsSync(path)) {
+    const json: string = await readFile(path, { encoding: 'utf-8' });
+
+    let cashii = JSON.parse(json) as Cashii;
+    cashii = { ...DEFAULT_CASHII, ...cashii };
+
+    // Convert ITransaction objs to Transactions.
+    let ts: Transaction[] = [];
+    cashii.transactions.forEach(t => {
+      ts.push(new Transaction(t));
+    });
+    cashii.transactions = ts;
+
+    return cashii;
+  }
+  else {
+    return DEFAULT_CASHII;
+  }
+}
+/**
+ * Saves a Cashii object to the path as JSON.
+ * 
+ * @param path 
+ * @param cashii 
+ */
+export async function save(cashii: Cashii, path?: string): Promise<void> {
+  path = path || DEFAULT_PATH;
+  const json = JSON.stringify(cashii);
+  await writeFile(path, json, { encoding: 'utf-8' });
+}
+export const cashii = await load();
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,54 @@
 #! /usr/bin/env node
-console.log("hello world!");
+import { load, save } from './cashii.js';
+import { program } from 'commander';
+import { format, parse } from "date-fns";
+import { Transaction, Trigger } from "./Transaction.js";
+
+// Load cashii data.
+const cashii = await load();
+
+// Basic info.
+program
+  .name('cashii')
+  .description('A CLI budgeting app.')
+  .version('0.1.0');
+
+// Transaction manipulation.
+program.command('transfer')
+  .argument('<memo>', 'What to call the transaction.')
+  .argument('<amount>', 'The amount to transfer.')
+  .argument('<date>', 'The date of the first transfer (dd/MM/yyyy)')
+  .argument('[trigger]', 'How the transfer is triggered', 'ONCE')
+  .action(async (memo: string, amount: string, date: string, trigger: Trigger) => {
+    trigger = trigger.toUpperCase() as Trigger;
+    if (!Object.values(Trigger).includes(trigger)) {
+      trigger = Trigger.ONCE;
+    }
+
+    cashii.transactions.push(new Transaction({
+      memo, 
+      amount: parseFloat(amount), 
+      date: parse(date, 'MM/dd/yyyy', new Date()),
+      trigger: trigger
+    }));
+    await save(cashii);
+  });
+
+program.command('remove')
+  .alias('rm')
+  .argument('<id>', 'The transaction ID (see list).')
+  .action(async (id: string) => {
+    cashii.transactions.splice(parseInt(id), 1);
+    await save(cashii);
+  });
+
+// List command
+program.command('list')
+  .action(() => {
+    cashii.transactions.forEach((t, i) => {
+      console.log(`${i} => ${format(t.date, 'MM/dd/yyyy')} | ${t.memo}: ${t.amount} - ${t.trigger}`);
+    });
+  });
+
+// Run program.
+program.parse();
